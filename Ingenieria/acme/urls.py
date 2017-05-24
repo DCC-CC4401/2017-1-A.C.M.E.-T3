87b740a19baa53d4b1d031fc7cd858a877135c37
Gabriel Azocar <gabriel@startnet.co>
2017-05-24 00:35:48 -0400

    init

---
+++ Ingenieria/acme/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'), #se asigna view llamada post_list al url ^$
+    url(r'^login/', views.login, name='login'),  # se asigna view llamada post_list al url ^$
+    url(r'^signup/', views.signup, name='signup'),
+]
---
+++ Ingenieria/acme/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'acme/init.html', {}) #va a construir lo puesto en la planilla .html senhalada
+
+def login(request):
+    return render(request, 'acme/login.html', {}) #va a construir lo puesto en la planilla .html senhalada
+
+def signup(request):
+    return render(request, 'acme/signup.html', {}) #va a construir lo puesto en la planilla .html senhalada
+
