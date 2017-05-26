14023cc401ce43f7900fa372ac2db97fbfbcafd6
Alexandra <alexasofia@hotmail.com>
2017-05-26 17:23:57 -0400

    add avatar to models

--- Ingenieria/acme/forms.py
+++ Ingenieria/acme/forms.py
@@ -4,50 +4,11 @@ from django import forms
 from django.core.files.images import get_image_dimensions
 from acme.models import ClientProfile, VendedorFijoProfile, VendedorAmbProfile
 
-
-# class UserProfileForm(forms.ModelForm):
-#    class Meta:
-#        model = UserProfile
-
-#    def clean_avatar(self):
-#        avatar = self.cleaned_data['avatar']
-
-#        try:
-#            w, h = get_image_dimensions(avatar)
-
-# validate dimensions
-#            max_width = max_height = 100
-#            if w > max_width or h > max_height:
-#                raise forms.ValidationError(
-#                    u'Please use an image that is '
-#                     '%s x %s pixels or smaller.' % (max_width, max_height))
-
-# validate content type
-#            main, sub = avatar.content_type.split('/')
-#            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
-#                raise forms.ValidationError(u'Please use a JPEG, '
-#                    'GIF or PNG image.')
-
-# validate file size
-#            if len(avatar) > (20 * 1024):
-#                raise forms.ValidationError(
-#                    u'Avatar file size may not exceed 20k.')
-
-#        except AttributeError:
-#            """
-#            Handles case when we are updating the user profile
-#            and do not supply a new avatar
-#            """
-#            pass
-
-#        return avatar
-
 class VendAmbForm(UserCreationForm):
     email = forms.EmailField(required=True)
     username = forms.CharField(max_length=200, required=True)
     first_name = forms.CharField(max_length=200, required=True)
     last_name = forms.CharField(max_length=200, required=True)
-    # avatar = forms.ImageField(upload_to=upload_location,default=1,blank=True)
     password1 = forms.CharField(widget=forms.PasswordInput)
     cash = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
     debit = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
@@ -66,8 +27,6 @@ class VendAmbForm(UserCreationForm):
         client = VendedorAmbProfile(user=user, cash=self.cleaned_data['cash'],
                                     debit=self.cleaned_data['debit'], credit=self.cleaned_data['credit'],
                                     student=self.cleaned_data['student'])
-        # if client.avatar:
-        #    client.set_avatar()
         if commit:
             client.save()
         return user
@@ -85,7 +44,6 @@ class VendFijoForm(UserCreationForm):
     username = forms.CharField(max_length=200, required=True)
     first_name = forms.CharField(max_length=200, required=True)
     last_name = forms.CharField(max_length=200, required=True)
-    # avatar = forms.ImageField(upload_to=upload_location,default=1,blank=True)
     password1 = forms.CharField(widget=forms.PasswordInput)
     init_time = forms.TimeField(required=True)
     end_time = forms.TimeField(required=True)
@@ -108,8 +66,6 @@ class VendFijoForm(UserCreationForm):
                                      end_time=self.cleaned_data['end_time'], cash=self.cleaned_data['cash'],
                                      debit=self.cleaned_data['debit'], credit=self.cleaned_data['credit'],
                                      student=self.cleaned_data['student'])
-        # if client.avatar:
-        #    client.set_avatar()
         if commit:
             client.save()
         return user
@@ -122,34 +78,23 @@ class VendFijoForm(UserCreationForm):
         return password1
 
 
-def upload_location(instance, filename):
-    PostModel = instance.__class__
-    new_id = PostModel.objects.order_by("id").last().id + 1
-    return "%s/%s" % (new_id, filename)
-
-
 class UserForm(UserCreationForm):
     # Nuevos campos escritos deben ser convertidos en form
     email = forms.EmailField(required=True)
     username = forms.CharField(max_length=200, required=True)
     first_name = forms.CharField(max_length=200, required=True)
     last_name = forms.CharField(max_length=200, required=True)
-    # avatar = forms.ImageField(upload_to=upload_location,default=1,blank=True)
     password1 = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
-        # fields = ('username', 'first_name', 'last_name', 'email', 'password')
 
     def save(self, commit=True):
-        # client = ProfileUser(super(UserForm, self).save(commit = False),forms.ImageField(upload_to=upload_location,default=1,blank=True))
         user = super(UserForm, self).save(commit=True)
         password = self.clean_password()
         user.set_password(password)
         client = ClientProfile(user=user)
-        # if client.avatar:
-        #    client.set_avatar()
         if commit:
             client.save()
         return user
--- Ingenieria/acme/models.py
+++ Ingenieria/acme/models.py
@@ -6,21 +6,12 @@ from django.db.models import SET_NULL
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-def upload_location(instance, filename):
-    PostModel = instance.__class__
-    new_id = PostModel.objects.order_by("id").last().id + 1
-    return "%s/%s" % (new_id, filename)
-
 class Product(models.Model):
     cost = models.PositiveIntegerField()
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=300)
     stock = models.PositiveIntegerField()
-    #avatar = models.ImageField(upload_to=upload_location)
-
-
-class Payment(models.Model):
-    name = models.CharField(max_length=30)
+    avatar = models.ImageField(default="acme/img/pollo1.png")
 
 
 class VendedorFijoProfile(models.Model):
@@ -28,42 +19,26 @@ class VendedorFijoProfile(models.Model):
     init_time = models.TimeField()
     end_time = models.TimeField()
     likes = models.PositiveIntegerField(default=0)
-    #avatar = models.ImageField(upload_to=upload_location)
-    dishes = models.ManyToManyField(Product)
+    avatar = models.ImageField(default="acme/img/AvatarVendedor1.png")
+    dishes = models.ManyToManyField(Product, blank=True)
     cash = models.BooleanField(default=True)
     credit = models.BooleanField(default=False)
     debit = models.BooleanField(default=False)
     student = models.BooleanField(default=False)
 
-    def set_avatar(self):
-        _avatar = self.avatar
-        if not _avatar:
-            self.avatar = "acme/img/AvatarEstudiante.png"
-
 
 class VendedorAmbProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     check = models.BooleanField(default=False)
-    #avatar = models.ImageField(upload_to=upload_location)
-    dishes = models.ManyToManyField(Product)
+    avatar = models.ImageField(default="acme/img/AvatarVendedor4.png")
+    dishes = models.ManyToManyField(Product, blank=True)
     likes = models.PositiveIntegerField(default=0)
     cash = models.BooleanField(default=True)
     credit = models.BooleanField(default=False)
     debit = models.BooleanField(default=False)
     student = models.BooleanField(default=False)
 
-    def set_avatar(self):
-        _avatar = self.avatar
-        if not _avatar:
-            self.avatar = "acme/img/AvatarEstudiante.png"
-
-
 class ClientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to=upload_location)
-
-    def set_avatar(self):
-        _avatar = self.avatar
-        if not _avatar:
-            self.avatar = "acme/img/AvatarEstudiante.png"
+    avatar = models.ImageField(default="acme/img/AvatarEstudiante.png")
 
